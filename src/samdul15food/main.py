dcddeef649c3eb189781c84617eb1707de8d51c4
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-12 16:59:42 +0900

    edit dockerfile

--- src/samdul15food/main.py
+++ src/samdul15food/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from typing import Union
-import pickle
 from datetime import datetime
 import time
+import os
+import csv
 
 app=FastAPI()
 
@@ -15,4 +16,13 @@ def food(name: str):
     # 시간 구하기
     time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # 음식 이름과 시간을 csv로 저장 -> /code/data/food.csv
+
+    file_path = "~/code/data/food.csv"
+
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    with open(file_path, mode='a', newline='') as f:
+        writer=csv.writer(f)
+        writer.writerow([name, time])
+
     return {"food": name, "time": time}
